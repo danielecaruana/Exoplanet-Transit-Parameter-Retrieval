8723071cba4546587f5d21da1169d4c005300b04
EntangledBeing <danielecaruana2@gmail.com>
2023-04-07 00:58:23 +0200

    Renamed files and added task 2

--- mcmc_sine.py
+++ mcmc_sine.py
@@ -8,7 +8,7 @@ import multiprocessing
 
 def model(theta, x):
     a, b, c, d = theta
-    model_val = a + (b * np.sin(c * x) + d)
+    model_val = a + (b * (np.sin((c * x)+d)))
     return model_val
 
 
@@ -19,7 +19,7 @@ def ln_like(theta, x, y, y_err):
 
 def ln_prior(theta):
     a, b, c, d = theta
-    if 33.5 <= a <= 35.5 and 2.5 <= b <= 3.5 and 20 <= c <= 25 and 6 <= d <= 25:
+    if 30 <= a <= 33.5 and 2.5 <= b <= 3.5 and 23 <= c <= 26 and 6 <= d <= 11:
         return 0
     else:
         return -np.inf
@@ -33,7 +33,7 @@ def ln_prob(theta, x, y, y_err):
 
 
 def _p0(initial_val, n_dim_val, n_walkers_val):
-    return [np.array(initial_val) + [1e-4, 75e-3, 1e-3, 1e-2] * np.random.randn(n_dim_val) for i in
+    return [np.array(initial_val) + [1e-4, 75e-3, 1e-2, 1e-2] * np.random.randn(n_dim_val) for i in
             range(n_walkers_val)]
 
 
@@ -55,10 +55,10 @@ def mcmc(p0, n_walkers, n_iter, n_dim, ln_prob, data_tup, pool):
 df = pd.read_csv('mcmc_intro_group_5.csv')
 data = (df['x'], df['y'], df['y_err'])
 
-n_walkers = 300 #500
-n_iter = 20000 #50000
+n_walkers = 100
+n_iter = 3000
 
-initial = np.array([34.5, 3, 23, 8])
+initial = np.array([32.5, 3, 24, 8])
 n_dim = len(initial)
 
 p0 = _p0(initial, n_dim, n_walkers)
@@ -71,14 +71,13 @@ if __name__ == '__main__':
         # Corner Plot
         labels = ['a', 'b', 'c', 'd']
         # reader = emcee.backends.HDFBackend("savefile")
-        tau = sampler.get_autocorr_time()
         # burnin = int(2 * np.max(tau))
         # thin = int(0.5 * np.min(tau))
         # samples = reader.get_chain(discard=burnin, flat=True, thin=thin)
         # log_prob_samples = reader.get_log_prob(discard=burnin, flat=True, thin=thin)
         # log_prior_samples = reader.get_blobs(discard=burnin, flat=True, thin=thin)
         print("Done")
-        fig = corner.corner(samples, show_titles=True, labels=labels, plot_datapoints=True, quantiles=[0.25, 0.5, 0.75])
+        fig = corner.corner(samples, show_titles=True, labels=labels, plot_datapoints=True, quantiles=[0.32, 0.5, 0.68])
         plt.savefig("corner.png")
 # Value Checking from plot
 
